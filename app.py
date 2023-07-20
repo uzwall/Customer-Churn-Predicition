3d46c7d017d65bb4e36bd283570675aab32e0b75
uzwalll <baraluzwal@gmail.com>
2023-07-20 10:27:00 +0545

    Updated and Model Loaded

--- app.py
+++ app.py
@@ -1,32 +1,53 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
-
+import joblib
 
 app = FastAPI()
 
+# Load the model
+model = joblib.load('model\prediction2.joblib')
+
+    
+class Customer(BaseModel):
+    AccountWeeks: float
+    ContractRenewal: int
+    DataPlan: int
+    DataUsage: float
+    CustServCalls: int
+    DayMins: float
+    DayCalls: int
+    MonthlyCharge: float
+    OverageFee: float
+    RoamMins: float
+
 
 @app.get("/home")
-async def root():
-    print( {'response':"200","message": "Hello World"})
-    return {'response':"200","message": "Hello World"}
+def home():
+    print("hello")
+    return {"message": "Hello World"}
+
+
+#function to print the churn prediction using trained model
+@app.post("/infer")
+def infer(data:Customer):
+    
+    # Convert the data to a 2D array
+    input_data = [[
+        data.AccountWeeks, data.ContractRenewal, data.DataPlan,
+        data.DataUsage, data.CustServCalls, data.DayMins,
+        data.DayCalls, data.MonthlyCharge, data.OverageFee,
+        data.RoamMins
+    ]]
+    
+    # Make predictions using the model
+    predictions = model.predict(input_data)
+    
+    if predictions[0]==1:
+        return {"Prediction":"Customer is more likely to churn"}
+    else:
+        return {"Predictions":"Customer will not churn"}
+
 
 if __name__=="__main__":
-    uvicorn.run("app:app",host="localhost",port=8080,reload=True)
-
-
-class Employee(BaseModel):
-    name: str
-    salary: float=10000.0
-    age: int
-
-
-@app.post("/employee")
-async def insert(emp:Employee):
-    print(emp)
-    return {"response":"200","message":"inserted successfully"}
-
-
-
-
-
+    uvicorn.run("app:app",host="localhost", port=8084, reload=True)
